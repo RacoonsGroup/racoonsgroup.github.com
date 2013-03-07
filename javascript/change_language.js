5e5f5338dfa7cc484841a1fe16580be8d3952c5b
cy6eria <e.n.gundorov@gmail.com>
2013-03-07 19:57:26 +0400

    Changes

--- javascript/change_language.js
+++ javascript/change_language.js
@@ -1,11 +1,45 @@
-function to_russian(){
-	$('#navigation_en').hide();
-	$('#navigation').show();
-}
+jQuery(function($) {
+    var LangSwitcher =  {
+        init: function() {
+            this.bindEvents()
+            
+            if (localStorage.getItem('lang') == null)
+                this.saveLang('ru')
+            if (localStorage.getItem('lang') == 'en')
+                $('body').load('/index_en.html')
+        },
 
-function to_english(){
-	$('#navigation').hide();
-	$('#navigation_en').show();
-}
+        bindEvents: function() {
+            $('.switch-lang').on('click', this.changeLang)
+        },
 
+        changeLang: function() {
+            if (!$(this).hasClass('active-lang')) {
+                var lang = $(this).text()
+                $(this)
+                    .addClass('active-lang')
+                    .parent()
+                    .siblings()
+                    .find('.switch-lang')
+                    .removeClass('active-lang')
 
+                if (lang == 'ru') {
+                    $('body').load('/index_ru.html')
+                } else {
+                    $('body').load('/index_en.html')
+                }
+
+                LangSwitcher.saveLang(lang)
+            }
+
+            return false
+        },
+
+        saveLang: function(lang) {
+            localStorage.setItem('lang', lang)
+        }
+
+    }
+
+    LangSwitcher.init()
+})
